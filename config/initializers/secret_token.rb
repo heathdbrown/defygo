56799f887dcc48aa65418730f8e8a2f6439d85ad
Andy H <hillad@gmail.com>
2011-02-14 20:17:09 -0600

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Defygo::Application.config.secret_token = '778508a7f1b9fc9918a5e0a6b88fc09194dfd1bbaf21a31cf7c2492bfb6f8f93026ef042aee05058909c20b07c46b58448028975d72ada28a8c6c1f345d97f95'
